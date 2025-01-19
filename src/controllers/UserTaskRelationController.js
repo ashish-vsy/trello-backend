10d878f45d6b0a2d8abbc76cd2c5ef8154cb1114
ashish-vsy <ashishkrobo@gmail.com>
2025-01-19 12:36:13 +0530

    chnaged user, task controller, and main index

--- src/controllers/UserTaskRelationController.js
+++ src/controllers/UserTaskRelationController.js
@@ -12,3 +12,19 @@ exports.addUserToTask = async (req, res) => {
         return res.status(500).json({ message: error.message })
     }
 }
+
+
+exports.getUserTasks = async (req, res) => {
+    try {
+        const { data, error } = await supabase
+            .from('usertaskrelation')
+            .select('*')
+            .eq('userid', req.params.id)
+        if (error) {
+            return res.status(500).json({ message: error.message })
+        }
+        return res.status(200).json({ data })
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+}
